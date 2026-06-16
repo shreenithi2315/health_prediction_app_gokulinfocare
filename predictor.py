1330c13e1f25367e9a6b42e52ab587f3b481d47d
shree kesavan <shreenithi2315@gmail.com>
2026-06-16 14:17:35 +0530

    connected groq api

---
+++ predictor.py
@@ -0,0 +1,21 @@
+from groq import Groq
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+my_client = Groq(api_key=os.getenv("Groq_api_key"))
+
+def get_prediction(name, glucose, haemoglobin, cholesterol):
+    my_prompt = f"""
+    Patient Name: {name}
+    Glucose: {glucose} mg/dL
+    Haemoglobin: {haemoglobin} g/dL
+    Cholesterol: {cholesterol} mg/dL
+    Write a short health remark in 2 to 3 plain sentences only.
+    No headings, no bullet points. Just a simple health observation.
+    """
+    result = my_client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[{"role": "user", "content": my_prompt}]
+    )
+    return result.choices[0].message.content
